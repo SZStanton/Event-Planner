abe02f4aa3a87dc7f98a5d758e3172ff6e24f7d7
SZStanton <ssebastianbusiness@gmail.com>
2026-05-16 10:51:51 +0200

    App.jsx updated, routes added, imports fixed in main.jsx, ProtectedRoutes type fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,81 @@
-import { useState } from 'react';
-import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
+// Context
+import { AuthProvider } from './context/AuthContext';
+import { EventsProvider } from './context/EventsContext';
+
+// Pages
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import AddEvent from './pages/AddEvent';
+import EditEvent from './pages/EditEvent';
+import Help from './pages/Help';
+
+// Components
+import Header from './components/Header';
+import ProtectedRoute from './components/ProtectedRoute';
+
+// Main app structure + routing
 function App() {
-  return <></>;
+  return (
+    <AuthProvider>
+      <EventsProvider>
+        <BrowserRouter>
+          {/* Always visible header */}
+          <Header />
+
+          <div className="container py-4">
+            <Routes>
+              {/* Public Routes */}
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+
+              {/*Protected Routes*/}
+              <Route
+                path="/"
+                element={
+                  <ProtectedRoute>
+                    <Dashboard />
+                  </ProtectedRoute>
+                }
+              />
+
+              <Route
+                path="/add"
+                element={
+                  <ProtectedRoute>
+                    <AddEvent />
+                  </ProtectedRoute>
+                }
+              />
+
+              <Route
+                path="/edit/:id"
+                element={
+                  <ProtectedRoute>
+                    <EditEvent />
+                  </ProtectedRoute>
+                }
+              />
+
+              <Route
+                path="/help"
+                element={
+                  <ProtectedRoute>
+                    <Help />
+                  </ProtectedRoute>
+                }
+              />
+
+              {/* Fallback Route */}
+              <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </EventsProvider>
+    </AuthProvider>
+  );
 }
 
 export default App;
